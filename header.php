a4b58a5d07862357a6b56db1ae269fb1be66b4fa
Gustavo de Assis Vilarino <132522283+Gustavo01081996@users.noreply.github.com>
2024-09-26 23:06:04 -0300

    inserindo templates de header e footer

---
+++ header.php
@@ -0,0 +1,33 @@
+<?php
+
+include_once ("helpers/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="<?= $BASE_URL ?>/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="<?= $BASE_URL ?>/js/additional-methods.js"></script>
+    <script type="text/javascript" src="<?= $BASE_URL ?>/js/localization/messages_pt_BR.js"></script>
+
+    <title>Construsite Brasil</title>
+</head>
+
+<body>
+    <header>
+        <div class="content-img">
+            <img src="<?= $BASE_URL ?>/images/logo_construsite.png" alt="Logotipo da Construsite Brasil">
+        </div>
+    </header>
